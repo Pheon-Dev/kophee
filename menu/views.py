cec805cc3f55653f971c4530d10f56dbcf24005a
Pheon-Dev <devpheon@gmail.com>
2023-04-27 18:11:29 +0300

    feat: templates and base html

--- menu/views.py
+++ menu/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'home.html', {})
