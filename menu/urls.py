d5c30d9f557f486a3f8d1e00e63fbd1c2b341569
Pheon-Dev <devpheon@gmail.com>
2023-04-27 19:08:05 +0300

    feat: authentication

--- menu/urls.py
+++ menu/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout')
 ]
--- menu/views.py
+++ menu/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
     # return HttpResponse("Hello, world. You're at the polls index.")
     return render(request, 'home.html', {})
+
+def login_user(request):
+    pass
+
+def logout_user(request):
+    pass
