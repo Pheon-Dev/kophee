2d3b72084600d22c42feb6a8105851d0dfec1059
Pheon-Dev <devpheon@gmail.com>
2023-04-29 13:59:52 +0300

    chore: restructuring the models

--- menu/admin.py
+++ menu/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import User, Table, Order, Menu
+from .models import Table, Order, Menu
 
-admin.site.register(User)
 admin.site.register(Table)
 admin.site.register(Order)
 admin.site.register(Menu)
--- menu/models.py
+++ menu/models.py
@@ -1,34 +1,28 @@
 from django.db import models
 
-class User(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.CharField(max_length=100)
-    phone = models.CharField(max_length=15)
-    address = models.CharField(max_length=100)
-    city = models.CharField(max_length=50)
-    id_number = models.CharField(max_length=50)
-    role = models.CharField(max_length=50)
-
-    def __str__(self):
-        return(f"{self.first_name} {self.last_name}")
-    
-    
 class Table(models.Model):
     table_id = models.CharField(max_length=50)
     capacity = models.IntegerField()
+
+    def __str__(self):
+        return(f"{self.table_id} {self.capacity}")
     
     
 class Menu(models.Model):
-    name = models.CharField(max_length=50)
+    item = models.CharField(max_length=50)
     price = models.IntegerField()
     description = models.CharField(max_length=200)
     image = models.ImageField(upload_to='images/')
     
+    def __str__(self):
+        return(f"{self.item} {self.price} {self.description} {self.image}")
+    
 class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
     table = models.ForeignKey(Table, on_delete=models.CASCADE)
-    waiter = models.ForeignKey(User, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     menu_item = models.TextField()
+    
+    def __str__(self):
+        return(f"{self.table} {self.menu_item} {self.quantity}")
     
